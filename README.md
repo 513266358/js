f834c5676bb9ddf0f9531299d76d72cf42a6ff40
lsz <shizhou.liu@szjlxh.com>
2016-03-12 09:08:42 +0800

    add ajaxfunction

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-11111
+##ajaxFunction
+```
+param url
+param data
+param sucessfunction(function())
+param datatype
+param type('POST' OR 'GET')
+param ansycstate (true or false) 
+param errorcallback( function())
---
+++ lsz.js
@@ -0,0 +1,49 @@
+          $.ajaxFunction = {
+                "ajax" : function (url, data, fun, style, typeState, asyncState, errcallback) {
+            console.group('ajax信息');
+            if (!url || !typeof (url) === 'string') {
+                console.error('地址不存在');
+            }
+
+            if (!fun || !typeof (fun) === 'function') {
+                console.error('方法不存在');
+            }
+
+            if (!typeState || typeState === '')
+            {
+                typeState = 'POST';
+                console.info('默认了为POST');
+            }
+            if (typeof (asyncState) !== 'boolean' && asyncState !== false) {
+                asyncState = true;
+                console.info('默认了为异步');
+            }
+
+            if (typeof (style) !== 'string') {
+                style = 'json';
+                console.info('默认格式为JSON');
+            }
+
+            $.ajax({
+                cache: true,
+                url: url,
+                data: data,
+                async: asyncState,
+                type: typeState,
+                dataType: style,
+                success: function (data) {
+                    var ajaxData = data;
+                    fun(ajaxData);
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    if (typeof (errcallback) === 'function') {
+                        errcallback([XMLHttpRequest, textStatus, errorThrown]);
+                    }
+                    console.error([JSON.stringify(XMLHttpRequest), JSON.stringify(textStatus), JSON.stringify(errorThrown)]);
+                }
+            });
+            console.groupEnd();
+            return this;
+        }
+            }
+
